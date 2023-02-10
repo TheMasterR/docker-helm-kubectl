c058d7ebc1b2706cb9fa7375b5d6601ea2ba4a69
Lucian Pantea <lucian.pantea@creatopy.com>
2023-02-10 13:09:38 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t themasterr/docker-helm-kubectl:helm-3.2.0 .
-docker tag themasterr/docker-helm-kubectl:helm-3.2.0 themasterr/docker-helm-kubectl:latest
+docker build -t themasterr/docker-helm-kubectl:helm-3.11.1 .
+docker tag themasterr/docker-helm-kubectl:helm-3.11.1 themasterr/docker-helm-kubectl:latest
 
-docker push themasterr/docker-helm-kubectl:helm-3.2.0
+docker push themasterr/docker-helm-kubectl:helm-3.11.1
 docker push themasterr/docker-helm-kubectl:latest
