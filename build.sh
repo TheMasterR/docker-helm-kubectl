dc93b9c249663202e9a9a15db53d5ee430b8591e
Lucian Pantea <lucian.pantea@creatopy.com>
2023-02-10 13:15:49 +0200

    update image

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker buildx build --push --platform linux/arm64,linux/amd64 --tag themasterr/docker-helm-kubectl:latest .
 
-docker buildx build --push --platform linux/arm64,linux/amd64 --tag themasterr/docker-helm-kubectl:3.11.1 .
+docker buildx build --push --platform linux/arm64,linux/amd64 --tag themasterr/docker-helm-kubectl:helm-3.11.1 .
